9c58ba20df300bf109b4fd105ce646c0342c684e
Carolyn Huynh & Justin Pacubas <student@epicodus.com>
2014-08-26 16:16:02 -0700

    Add ability to create new sighting.  Need to work on linking new sighting page.  WIP.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,16 @@
 class SightingsController < ApplicationController
-  def show
-    @specie = Specie.find(params[:id])
+  def new
+    specie = Specie.find(params[:id])
+    @sighting = specie.sightings.new
+    render('sightings/new.html.erb')
   end
 
+  def create
+    @sighting = Sighting.new(params[:sighting])
+    if @sighting.save
+      render('sightings/success.html.erb')
+    else
+      render('sightings/new.html.erb')
+    end
+  end
 end
